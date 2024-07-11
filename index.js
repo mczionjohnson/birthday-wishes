1a877a7b2cc2a978be96fc5b44d23e9341826216
mczionjohnson <mczionjohnson@gmail.com>
2024-07-11 13:37:29 +0100

    schedule

--- index.js
+++ index.js
@@ -117,7 +117,8 @@ let allCelebrants = (array) => {
 };
 
 //actual cron job function
-schedule.scheduleJob("0 7 * * *", async () => {
+schedule.scheduleJob("00 07 * * *", async () => {
+  // schedule.scheduleJob("*/2 * * * *", async () => {
   // cron job every 07:00am
 
   //find in db or array
